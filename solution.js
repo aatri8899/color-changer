cf6df1fd8cc002808fd86a8fb07b81b526c9d0a4
aatri8899 <aatri8899@withs.org>
2016-04-20 13:59:36 +0000

    color-changer

--- solution.js
+++ solution.js
@@ -1,1 +1,17 @@
+var colors = ["yellow", "purple", "green", "black"];
+function changeColor () {
+    var randomIndex = Math.floor((Math.random() * colors.length) + 0);
+    $("body").css("background-color", colors [randomIndex]);
+}
 
+function addColor(){
+  var newColor= $("#newColor").val();
+  colors[colors.length]= newColor;
+  $("body").css("background",  colors[colors.length]);
+}
+
+     $(document).ready(function(){
+         $("#change").click(changeColor);
+         $("#add").click(addColor);
+     });   
+    
